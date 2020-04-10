eed316d5be99d81f4d25690acf5a7d8c7a9abcf3
Rexsunon <osikhenaevidence@gmail.com>
2020-04-10 02:27:54 +0100

    Add routers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,18 @@ export const applyMiddleware = (
     wrapper(router);
   }
 };
+
+type Handler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void> | void;
+
+type Route = { path: string; method: string; handler: Handler | Handler[] };
+
+export const applyRoute = (routes: Route[], router: Router) => {
+  for (const route of routes) {
+    const { path, method, handler } = route;
+    (router as any)[method](path, handler);
+  }
+};
